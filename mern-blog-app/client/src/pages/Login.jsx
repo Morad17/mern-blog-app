f31873cbffeb97ac11a284d9c556a2454f286381
Morad17 <morad123@hotmail.co.uk>
2023-04-23 22:33:46 +0100

    added login func, sorted out .env file

--- mern-blog-app/client/src/pages/Login.jsx
+++ mern-blog-app/client/src/pages/Login.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = (e) => {
+    e.preventDefault();
+    async fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type': 'application/json'}
+  }
   return (
-    <form action="">
-      <input type="text" placeholder="username" />
-      <input type="text" placeholder="password" />
+    <form action="" onSubmit={login}>
+      <h1>Login</h1>
+      <input type="text" placeholder="username" value={username} onChane={e => setUsername(e.target.value)}/>
+      <input type="text" placeholder="password" value={password} onChane={e => setPassword(e.target.value)}/>
     </form>
   )
 }
--- mern-blog-app/server/index.js
+++ mern-blog-app/server/index.js
@@ -3,15 +3,26 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User.js')
+const bcrypt = require('bcryptjs')
 
+const salt = bcrypt.genSaltSync(10)
+
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGOOSE_URI)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.post('/register', async (req,res)=> {
-    const {username, password} = req.body
-    const userDoc = await User.create({ username, password})
+    const { username, password: password} = req.body
+    const userDoc = await User.create({ username, password:bcrypt.hashSync(password, salt)})
+    res.json(userDoc)
+})
+
+app.post('/login', async (req,res)=> {
+    const { username, password } = req.body
+    const userDoc = await User.findOne({username})
+    const passOk = bcrypt.compareSync(password, userDoc.password)
     res.json(userDoc)
 })
 
