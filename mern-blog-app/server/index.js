443a04fa1e921c100066d1460a000c021cfb8def
Morad17 <morad123@hotmail.co.uk>
2023-05-11 08:06:55 +0100

    update on index port

--- mern-blog-app/server/index.js
+++ mern-blog-app/server/index.js
@@ -4,13 +4,16 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User.js')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const salt = bcrypt.genSaltSync(10)
+const secret = 'sdfasdfasdfasdfads'
 
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+const PORT = 4000
 mongoose.connect(process.env.MONGODB_URI)
 
 app.post('/register', async (req,res)=> {
@@ -23,8 +26,20 @@ app.post('/login', async (req,res)=> {
     const { username, password } = req.body
     const userDoc = await User.findOne({username})
     const passOk = bcrypt.compareSync(password, userDoc.password)
-    res.json(passOk)
+    if(passOk){
+
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err 
+            res.json(token)
+
+        })
+       
+    } else {
+        res.status(400).json('wrong credentials')
+    }
 })
 
-app.listen(4000) 
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+}) 
 
